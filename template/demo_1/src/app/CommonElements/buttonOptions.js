47386c8f67730d45d4f898a91071f917e51f0068
Ada <adatanyindawn@gmail.com>
2020-10-17 14:37:35 +0800

    edit profile changes

--- template/demo_1/src/app/CommonElements/buttonOptions.js
+++ template/demo_1/src/app/CommonElements/buttonOptions.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
 
+const selectedStyle = {opacity: 1 , margin: 3}
+const disabledStyle = {opacity: 0.5, margin: 3} 
+
 export class ButtonOptions extends Component {
     constructor(props) {
         super(props);
       }
+
+    onClick = event => {
+        if (this.props.selectedList.includes(event.target.value)){
+            this.props.onChange(this.props.selectedList.filter((selectedOption) => selectedOption !== event.target.value))
+        }
+        else {
+        this.props.onChange([... this.props.selectedList, event.target.value])
+    }
+      };
 
     render() {
         var divStyle = {
@@ -11,8 +23,10 @@ export class ButtonOptions extends Component {
         }
         return (
             <div style={divStyle}>
-                {this.props.optionsList.map((option) => <button type="button" className="btn btn-outline-dark  btn-icon-text">
-                    {option}
+                {this.props.optionsList.map((option) => 
+                <button key={option.option} value={option.option} style={this.props.selectedList ? this.props.selectedList.includes(option.option) ? selectedStyle: disabledStyle : disabledStyle} type="button" class="btn btn-outline-dark  btn-icon-text" onClick={this.onClick}>
+                    {option.icon ? <i className={option.icon}></i> : null}
+                    {option.option}
                 </button>)}
             </div>
         )
--- template/demo_1/src/app/UserProfile/editUserProfile.js
+++ template/demo_1/src/app/UserProfile/editUserProfile.js
@@ -1,18 +1,76 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { FirebaseContext } from '../Firebase';
 import { Form } from 'react-bootstrap';
 import * as ROUTES from '../../constants/routes';
 import { withFirebase } from '../Firebase';
 import { withAuthUser } from '../Session';
 import { Link } from 'react-router-dom';
-import InstrumentOptions from '../CreatePosts/instrumentOptions';
-import GenreOptions from '../CreatePosts/genreOptions';
 import ButtonOptions from '../CommonElements/buttonOptions';
 
+const instrumentOptions =
+    [{ "option": 'Electric Guitar', "icon": "mdi mdi-guitar-electric btn-icon-prepend mdi-24px" },
+    { "option": 'Acoustic Guitar', "icon": "mdi mdi-guitar-acoustic btn-icon-prepend mdi-24px" },
+    { "option": 'Drums', "icon": "mdi mdi-music-circle btn-icon-prepend mdi-24px" },
+    { 'option': 'Keys', "icon": "mdi mdi-music-circle btn-icon-prepend mdi-24px" },
+    { "option": 'Vocals', "icon": "mdi mdi-microphone-variant btn-icon-prepend mdi-24px" },
+    { "option": 'Others', "icon": "mdi mdi-saxophone btn-icon-prepend mdi-24px" }
+    ];
+
+const preferredGenreOptions =
+    [{ "option": 'Rock' },
+    { "option": 'Alternative' },
+    { "option": 'Pop' },
+    { 'option': 'Punk' },
+    { "option": 'Indie' },
+    { "option": 'Jazz' },
+    { "option": 'Classical' },
+    { "option": 'Others' }
+    ];
+
+const skillOptions =
+    [{ "option": 'Beginner' },
+    { "option": 'Amateur' },
+    { "option": 'Intermediate' },
+    { 'option': 'Good' },
+    { "option": 'Expert' }];
+
+const performanceExperienceOptions =
+    [{ "option": 'None' },
+    { "option": 'A long time ago' },
+    { "option": 'Few' },
+    { 'option': 'Frequent' },];
+
+const playLengthOptions =
+    [{ "option": '< 1 Year' },
+    { "option": '1 - 2 Years' },
+    { "option": '3 - 5 Years' },
+    { 'option': '6 - 10 Years' },
+    { 'option': '> 10 Years' },];
+
+const INITIAL_STATE = {
+    selectedInstruments: [],
+    selectedGenres: [],
+};
+
+
 export class EditUserProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { ...INITIAL_STATE };
+      }
+
+    onSelectInstrument = (selectedInstruments) => {
+        this.setState({ selectedInstruments: selectedInstruments });
+      };
+    
+      onSelectGenre = (selectedGenres) => {
+        this.setState({ selectedGenres: selectedGenres });
+      };
+
     render() {
         return (
             <div>
+                {console.log(this.state)}
                 <div className="d-flex align-items-center auth px-0">
                     <div className="row w-100 mx-0">
                         <div className="col-lg-12 mx-auto">
@@ -25,21 +83,25 @@ export class EditUserProfile extends Component {
                                     <h5>Basic Information</h5>
                                     <span />
                                     <h6 className="font-weight-light">What instrument(s) do you play?</h6>
-                                    <InstrumentOptions />
+                                    <ButtonOptions optionsList={instrumentOptions} onChange={this.onSelectInstrument} selectedList={this.state.selectedInstruments}/>
                                     <h6 className="font-weight-light">Preferred genres</h6>
-                                    <GenreOptions />
+                                    <ButtonOptions optionsList={preferredGenreOptions} onChange={this.onSelectGenre} selectedList={this.state.selectedGenres}/>
                                     <h6 className="font-weight-light">Biography</h6>
                                     <Form.Group className="d-flex search-field">
                                         <Form.Control as="textarea" rows="2" placeholder="Describe yourself!" size="lg" className="h-auto" onChange={this.onChange} name="biography" />
                                     </Form.Group>
+                                    <h6 className="font-weight-light">Contact details</h6>
+                                    <Form.Group className="d-flex search-field">
+                                        <Form.Control as="textarea" rows="1" placeholder="Contact" size="lg" className="h-auto" onChange={this.onChange} name="spotify link" />
+                                    </Form.Group>
                                     <h5>Skills Level</h5>
                                     <h6 className="font-weight-light">What level would you describe yourself to be?</h6>
-                                    <ButtonOptions optionsList={['Beginner', 'Amature', 'Intermediate', 'Good', 'Expert']}/>
+                                    <ButtonOptions optionsList={skillOptions} />
                                     <h6 className="font-weight-light">Performance experience</h6>
-                                    <ButtonOptions optionsList={['None', 'A long time ago', 'Few', 'Frequent']}/>
+                                    <ButtonOptions optionsList={performanceExperienceOptions} />
                                     <h6 className="font-weight-light">How long have you been playing?</h6>
-                                    <ButtonOptions optionsList={['< 1 Year', '1 - 2 Years', '3-5 Years', '6 - 10 Years', '> 10 Years']}/>
-                                    <span/>
+                                    <ButtonOptions optionsList={playLengthOptions} />
+                                    <span />
                                     <h5>Preferences</h5>
                                     <h6 className="font-weight-light">Top 5 Favourite Artists</h6>
                                     <Form.Group className="d-flex search-field">
@@ -72,4 +134,4 @@ export class EditUserProfile extends Component {
     }
 }
 
-export default withFirebase(EditUserProfile);
+export default withFirebase(withAuthUser(EditUserProfile));
